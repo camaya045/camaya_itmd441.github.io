3f47c333235ed59374369cbd77315345172f1ec6
Carlos Amaya <camaya045@gmail.com>
2026-04-09 16:07:30 -0500

    adding js code and updating html

--- Lab5/script.js
+++ Lab5/script.js
@@ -1,3 +1,13 @@
+const billInput = document.getElementById("bill");
+const taxInput = document.getElementById("tax");
+const tipInput = document.getElementById("tip");
+const currencySelect = document.getElementById("toCurrency");
+const tipResultInput = document.getElementById("tipResult");
+const totalBillInput = document.getElementById("TotalBill");
+const convertButton = document.getElementById("convertButton");
+
+
+
 function convertUSDToEuros(usd) {
     const exchangeRate = 0.85; // 1 USD = 0.85 EUR
     return (usd * exchangeRate).toFixed(2);
@@ -11,3 +21,9 @@ function convertUSDToRupees(usd) {
 // Example usage
 console.log(`$100 USD = €${convertUSDToEuros(100)}`);
 console.log(`$100 USD = ₹${convertUSDToRupees(100)}`);
+
+convertButton.addEventListener("click", function() {
+    const billAmount = parseFloat(billInput.value);
+    const taxPercentage = parseFloat(taxInput.value);
+    const tipPercentage = parseFloat(tipInput.value);
+    const selectedCurrency = currencySelect.value;})
